b296862685d40ebf63d543329439efbfb03f50d0
Damilare Jolayemi <djolayemi@gmail.com>
2021-07-21 08:05:24 +0100

    features'

---
+++ src/main/java/com/solofund/SolofundApplication.java
@@ -0,0 +1,13 @@
+package com.solofund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolofundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolofundApplication.class, args);
+    }
+
+}
